ad31d5173c6cfddf6bc34b22f854f96ea346e0de
Richard Woollcott <richardwoollcott@hotmail.com>
2016-03-30 20:55:28 +0100

    Initial version using existing code

---
+++ iOS/ExtensionsMvvmLight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundation;
+using UIKit;
+
+namespace ObservableTables.iOS
+{
+	public static class ExtensionsMvvmLight
+	{
+		public static TaskListObservableTableViewController<T> GetTaskListController<T>(
+			this IList<T> list,
+			Func<NSString, UITableViewCell> createCellDelegate,
+			Action<UITableViewCell, T, NSIndexPath> bindCellDelegate)
+		{
+			return new TaskListObservableTableViewController<T>
+			{
+				DataSource = list,
+				CreateCellDelegate = createCellDelegate,
+				BindCellDelegate = bindCellDelegate
+			};
+		}
+	}
+}
+
---
+++ iOS/TaskListObservableTableSource.cs
@@ -0,0 +1,40 @@
+﻿using UIKit;
+using Foundation;
+
+namespace ObservableTables.iOS
+{
+	public class TaskListObservableTableSource<T> : ObservableTableSource<T>
+	{
+		public TaskListObservableTableSource(ObservableTableViewController<T> controller)
+			: base(controller)
+		{
+		}
+			
+		public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
+		{
+			return true;
+		}
+
+		public override void CommitEditingStyle(UITableView tableView,
+												UITableViewCellEditingStyle editingStyle,
+												NSIndexPath indexPath)
+		{
+			switch (editingStyle)
+			{
+				case UITableViewCellEditingStyle.Delete:
+					// remove the item from the underlying data source
+					_controller.DataSource.RemoveAt(indexPath.Row);
+					// No need to delete the row from the table as the tableview is bound to the data source
+					break;
+			}
+		}
+
+		public override UITableViewCellEditingStyle EditingStyleForRow(UITableView tableView,
+																		NSIndexPath indexPath)
+		{
+			return UITableViewCellEditingStyle.Delete;
+		}
+
+	}
+}
+
